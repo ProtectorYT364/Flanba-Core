a54ce2dec7e5f3324de2b4b4b4f90c147cc57ccf
Sergittos <sergittoscoc@gmail.com>
2021-12-25 16:38:40 +0100

    Initial structure

---
+++ src/flanbacore/FlanbaCore.php
@@ -0,0 +1,20 @@
+<?php
+/*
+* Copyright (C) Sergittos - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+*/
+
+declare(strict_types=1);
+
+
+namespace flanbacore;
+
+
+use pocketmine\plugin\PluginBase;
+
+class FlanbaCore extends PluginBase {
+
+    // TODO: Make scoreboard api
+
+}

ba5cfe4bcf91df83bce2b8b322d8aee9e65ded0f
Superomarking <71559522+Superomarking@users.noreply.github.com>
2022-01-14 13:19:13 +0300

    Added commands

---
+++ src/sergittos/flanbacore/command/tempc/HubCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+use sergittos\flanbacore\session\SessionFactory;
+
+class HubCommand extends \pocketmine\command\Command{
+
+	public function __construct(){
+		parent::__construct("hub", "Teleports you to the lobby!", null, ["lobby", "l", "spawn"]);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute(\pocketmine\command\CommandSender $sender, string $commandLabel, array $args){
+		$session = SessionFactory::getSession($sender);
+		if($session->hasMatch()){
+			$session->setMatch(null, true);
+		}
+	}
+}

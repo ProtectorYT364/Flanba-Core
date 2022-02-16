0101bf81a1d8d6f1574acab6ca951416fe73f91e
Superomarking <71559522+Superomarking@users.noreply.github.com>
2022-02-16 16:38:52 +0300

    Fixed spacing as its triggering my asssssssss

--- src/sergittos/flanbacore/match/FlanbaMatch.php
+++ src/sergittos/flanbacore/match/FlanbaMatch.php
@@ -163,7 +163,7 @@ class FlanbaMatch {
             } else {
                 $session->setScoreboard(new WaitingPlayersScoreboard($session, $this));
             }
-            $this->broadcastMessage("§l§a» §r§a§k{$session->getUsername()} §r§7joined [§8{$players_count}/{$max_players}]!");
+            $this->broadcastMessage("§l§a» §r§a§k{$session->getUsername()} §r§7 joined §8[{$players_count}/{$max_players}]!");
         }
 
         // TODO: Clean this

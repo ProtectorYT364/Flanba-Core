4dbb7a1d836adbb4de26f7086903a98909bf38aa
Marc <92360256+iMqrc@users.noreply.github.com>
2022-02-18 09:08:00 +0800

    Add files via upload

---
+++ src/sergittos/flanbacore/item/presets/match/UnlocksItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DidntPot\items\types;
+
+use DidntPot\items\LobbyItem;
+use pocketmine\item\ItemIds;
+use pocketmine\item\ItemUseResult;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+
+class UnlocksItem extends FlanbaItem {
+    
+    public function __construct()
+    {
+        parent::__construct("§6Unlocks §8[Use]", 450);
+    }
+
+    /**
+     * @param Player $player
+     * @param Vector3 $directionVector
+     * @return ItemUseResult
+     */
+    public function onClickAir(Player $player, Vector3 $directionVector): ItemUseResult
+    {
+        $player->sendMessage(TextFormat::RED . "Coming soon...");
+        return ItemUseResult::SUCCESS();
+    }
+}

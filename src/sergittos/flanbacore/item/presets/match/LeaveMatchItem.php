436729a6753cdd38f31b39ef26635088eeb2e6a7
BestCodrEver <BestCodrEver@users.noreply.github.com>
2022-02-19 20:36:06 +1100

    reset countdown when quit

--- src/sergittos/flanbacore/item/presets/match/LeaveMatchItem.php
+++ src/sergittos/flanbacore/item/presets/match/LeaveMatchItem.php
@@ -29,6 +29,9 @@ class LeaveMatchItem extends FlanbaItem {
 
     public function onClickAir(Player $player, Vector3 $directionVector): ItemUseResult {
         $session = SessionFactory::getSession($player);
+        if($session->getMatch()->getStage() === FlanbaMatch::COUNTDOWN_STAGE){
+            $session->getMatch()->setCountdown(10);
+        }
         $session->setMatch(null);
         $session->teleportToLobby();
         return ItemUseResult::SUCCESS();

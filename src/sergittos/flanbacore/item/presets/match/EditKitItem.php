5620818c0ad3be4d48d0c55e420d6d0e67cc7112
Superomarking <71559522+Superomarking@users.noreply.github.com>
2022-02-21 21:45:58 +0300

    Add files via upload

--- src/sergittos/flanbacore/item/presets/match/EditKitItem.php
+++ src/sergittos/flanbacore/item/presets/match/EditKitItem.php
@@ -15,8 +15,11 @@ use pocketmine\item\ItemIds;
 use pocketmine\item\ItemUseResult;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
 use sergittos\flanbacore\item\FlanbaItem;
+use sergittos\flanbacore\match\FlanbaMatch;
 use sergittos\flanbacore\session\SessionFactory;
+use sergittos\flanbacore\utils\ConfigGetter;
 
 class EditKitItem extends FlanbaItem {
 
@@ -25,7 +28,16 @@ class EditKitItem extends FlanbaItem {
     }
 
     public function onClickAir(Player $player, Vector3 $directionVector): ItemUseResult {
-        SessionFactory::getSession($player)->sendEditKitMenu();
+        $session = SessionFactory::getSession($player);
+		if($session->hasMatch()){
+			if($session->getMatch()->getStage() == 1){
+				if($session->getMatch()->getCountdown() < 4){
+					$session->getPlayer()->sendMessage(TextFormat::RED . "Actions discarded as the game is starting!");
+					return ItemUseResult::FAIL();
+				}
+			}
+		}
+		$session->sendEditKitMenu();
         return ItemUseResult::SUCCESS();
     }
 
